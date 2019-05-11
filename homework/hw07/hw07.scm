8ba31767766f162564c3b37ab8a083452d6df815
wongchihaul <wongchihaul@qq.com>
2019-05-11 22:57:34 +0800

    Week 10

--- homework/hw07/hw07.scm
+++ homework/hw07/hw07.scm
@@ -39,12 +39,22 @@
 (define (empty? s) (null? s))
 
 (define (add s v)
-    'YOUR-CODE-HERE
+    (cond
+      ((empty? s) (list v))
+      ((= (car s) v) s)
+      ((ordered? (cons v s)) (cons v s))
+      (else (cons (car s) (add (cdr s) v)))
     )
+)
+
 
 ; Sets as sorted lists
 (define (contains? s v)
     'YOUR-CODE-HERE
+    (cond
+      ((empty? s) False)
+      ((= (car s) v) True)
+      (else (contains? (cdr s) v) ))
     )
 
 ; Equivalent Python code, for your reference:
@@ -64,8 +74,13 @@
 
 (define (intersect s t)
     'YOUR-CODE-HERE
+    (cond
+      ((empty? s) s)
+      ((empty? t) t) 
+      ((= (car s) (car t)) (cons (car s) (intersect (cdr s) (cdr t)) ))
+      ((> (car s) (car t)) (intersect s (cdr t)))
+      (else (intersect (cdr s) t)))
     )
-
 ; Equivalent Python code, for your reference:
 ;
 ; def intersect(set1, set2):
@@ -82,4 +97,10 @@
 
 (define (union s t)
     'YOUR-CODE-HERE
+    (cond 
+      ((empty? s) t)
+      ((empty? t) s)
+      ((< (car s) (car t)) (add (union (cdr s) t) (car s) ))
+      ((> (car s) (car t)) (add (union (cdr t) s) (car t) ))
+      ((= (car s) (car t)) (add (union (cdr s) (cdr t)) (car s) )))
     )
